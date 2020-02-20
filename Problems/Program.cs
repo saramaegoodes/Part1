fb80354929bf679f98fd4012e3eb5a081db1c1ef
g18g5043 <g18g5043@HUG136>
2020-02-20 16:11:31 +0200

    Added .csproj file to repository

---
+++ Problems/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Problems
+{
+    public class Program
+    {
+        public static void Prefix(string input){
+            if (input == null){
+                Console.WriteLine("null");
+            }
+            else {
+                int a =0, numWords =1;
+                int string_length = input.Length;
+                while (a <= string_length -1){
+                    if(input[a]==' ' || input[a]=='\n' || input[a]=='\t'){
+                        numWords++;
+                    }
+                    a++;           
+                }
+                Console.WriteLine(string_length + "," + numWords + ":" + input);   
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string string_input;
+            string_input = Console.ReadLine();
+            Prefix(string_input);
+        }
+    }
+}
